7471d88d9a1533e8eb74d35e1c34c899ecb4b0cb
Quincy Maynor <quincymaynor@gmail.com>
2023-08-10 21:49:26 -0500

    added profile routes

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -2,16 +2,6 @@ const router = require('express').Router();
 const { Character } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const newCharacter = await Character.findAll();
-
-    res.status(200).json(newCharacter);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.post('/', withAuth, async (req, res) => {
   try {
     const newCharacter = await Character.create({
