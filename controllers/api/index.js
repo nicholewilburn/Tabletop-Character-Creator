10602648241c6f593f756037f064cceb3257e755
Quincy Maynor <quincymaynor@gmail.com>
2023-08-11 09:10:33 -0500

    connected profile routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const profileRoutes = require('./profileRoutes');
 const characterRoutes = require('./characterRoutes');
 
 router.use('/users', userRoutes);
+router.use('/profile', profileRoutes);
 router.use('/characters', characterRoutes);
 
 module.exports = router;
--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -11,3 +11,5 @@ router.get('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+module.exports = router;
