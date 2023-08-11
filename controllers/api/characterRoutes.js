c5142cd54582ac340579bc8e498275c41b1ea117
Quincy Maynor <quincymaynor@gmail.com>
2023-08-10 21:45:53 -0500

    fixed server start problem

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const { Character } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const newCharacter = await Character.findAll();
+
+    res.status(200).json(newCharacter);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newCharacter = await Character.create({
