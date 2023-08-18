9fe0bb0b477c2cc18297a20732a27265d6952353
Nichole <nicholewilburn@gmail.com>
2023-08-17 19:08:09 -0500

    portrait routes ?

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const profileRoutes = require('./profileRoutes');
 const characterRoutes = require('./characterRoutes');
+const portraitRoutes = require('./characterRoutes');
 
 router.use('/users', userRoutes);
 router.use('/profile', profileRoutes);
 router.use('/characters', characterRoutes);
+router.use('/portrait', portraitRoutes);
 
 module.exports = router;
--- controllers/api/portraitRoutes.js
+++ controllers/api/portraitRoutes.js
@@ -1,17 +1,45 @@
 const router = require('express').Router();
 // THIS WILL PROBABLY WANT A PORTRAIT MODEL TO AFFECT SPECIFICALLY??
 const { Character } = require('../../models');
-const withAuth = require('../../utils/auth');
 
 // THIS HAS THE CHARACTER CRUD OPERATIONS, SHOULD MAKE A GOOD TEMPLATE FOR THE PORTRAITS
 
+// get portrait page
+router.get('/', async (req, res) => {
+  try {
+    const characterData = await Character.findAll({
+      include: [
+        {
+          model: Character,
+          attributes: ['name', 'portrait_url'],
+        },
+      ],
+    });
 
+    const characters = characterData.map((character) => character.get({ plain: true }));
 
+    res.render('portrait', { 
+      ...characters, 
+      // logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// read character (add withAuth later?)
+// read portrait (add withAuth later?)
 router.get('/:id', async (req, res) => {
   try {
-      const characterData = await Character.findByPk(req.params.id);
+      const characterData = await Character.findByPk(req.params.id,
+        {
+          include: [
+            {
+              model: Character,
+              attributes: ['portrait_url'],
+            }
+          ],
+        }
+      );
 
       if (!characterData) {
           res.status(404).json({ message: 'No character found with that id!' });
@@ -24,24 +52,19 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// create character (add withAuth later?)
-router.post('/', async (req, res) => {
-  try {
-    const newCharacter = await Character.create({
-      ...req.body,
-      // user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newCharacter);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 // update character (add withAuth later?)
 router.put('/:id', async (req, res) => {
   try {
-      const characterData = await Character.findByPk(req.params.id);
+      const characterData = await Character.findByPk(req.params.idreq.params.id,
+        {
+          include: [
+            {
+              model: Character,
+              attributes: ['portrait_url'],
+            }
+          ],
+        }
+      );
       characterData.set({
         ...req.body,
         // user_id: req.session.user_id,
@@ -53,26 +76,5 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-// delete character (add withAuth later?)
-router.delete('/:id', async (req, res) => {
-  try {
-    const characterData = await Character.destroy({
-      where: {
-        id: req.params.id,
-        // user_id: req.session.user_id,
-      },
-    });
-
-    if (!characterData) {
-      res.status(404).json({ message: 'No character found with this id!' });
-      return;
-    }
-
-    res.status(200).json(characterData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
